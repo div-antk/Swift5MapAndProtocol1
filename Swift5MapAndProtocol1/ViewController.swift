d6d39559bf3d2dcf5ffc123132fa960746a37b88
div-antk <51283468+owaii@users.noreply.github.com>
2020-12-16 18:14:24 +0900

    Initial Commit

---
+++ Swift5MapAndProtocol1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift5MapAndProtocol1
+//
+//  Created by Takuya Ando on 2020/12/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
